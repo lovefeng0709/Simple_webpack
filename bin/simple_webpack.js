b7eb275c8865f5e1462db7d68fad7d6728c434b1
zujiaozhang <859166618@qq.com>
2020-06-21 21:45:49 +0800

    6m21d

--- bin/simple_webpack.js
+++ bin/simple_webpack.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log('222')
+
+// console.log('开始打包')
+
+const path = require('path')
+// 读取需要打包项目的配置文件
+let config = require(path.resolve('webpack.config.js'))
+console.log(config)
+const Compiler = require('..lib/compiler.js')
+
+new Compiler(config).start()
